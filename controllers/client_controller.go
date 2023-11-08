c8f45a4d2d19db871a0b90ea1d9bee9e52fa1ad3
Matthew Pfefferle <matthew.pfefferle@weka.io>
2023-11-08 13:41:17 -0500

    Create a busybox deployment

--- controllers/client_controller.go
+++ controllers/client_controller.go
@@ -18,8 +18,14 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -27,6 +33,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	wekav1alpha1 "github.com/weka/weka-operator/api/v1alpha1"
+)
+
+const (
+	typeAvailableClient   = "Available"
+	typeUnavailableClient = "Unavailable"
 )
 
 // ClientReconciler reconciles a Client object
@@ -61,7 +72,57 @@ func (r *ClientReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, err
 	}
 
-	logger.Info("Client", "name", client.Name, "namespace", client.Namespace)
+	if client.Status.Conditions == nil || len(client.Status.Conditions) == 0 {
+		meta.SetStatusCondition(&client.Status.Conditions, metav1.Condition{
+			Type:    typeAvailableClient,
+			Status:  metav1.ConditionUnknown,
+			Reason:  "Reconciling",
+			Message: "Reconciling Client",
+		})
+		if err = r.Status().Update(ctx, client); err != nil {
+			logger.Error(err, "unable to update Client status")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Deployment
+	deployment := &appsv1.Deployment{}
+	err = r.Get(ctx, req.NamespacedName, deployment)
+	if err != nil && apierrors.IsNotFound(err) {
+		// define a new deployment
+		dep, err := r.deploymentForClient(client)
+		if err != nil {
+			logger.Error(err, "unable to create Deployment for Client", "Client.Namespace", client.Namespace, "Client.Name", client.Name)
+
+			meta.SetStatusCondition(&client.Status.Conditions, metav1.Condition{
+				Type:    typeAvailableClient,
+				Status:  metav1.ConditionFalse,
+				Reason:  "Reconciling",
+				Message: fmt.Sprintf("Failed to create Deployment for the custom resource (%s): (%s)", client.Name, err),
+			})
+
+			if err = r.Status().Update(ctx, client); err != nil {
+				logger.Error(err, "unable to update Client status")
+				return ctrl.Result{}, err
+			}
+
+			return ctrl.Result{}, err
+		}
+
+		logger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		if err = r.Create(ctx, dep); err != nil {
+			logger.Error(err, "Failed to create new Deployment",
+				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{RequeueAfter: time.Minute * 1}, nil
+	} else if err != nil {
+		logger.Error(err, "unable to fetch Deployment")
+		return ctrl.Result{}, err
+	}
+
 	logger.Info("Finished Reconciling Client")
 	return ctrl.Result{}, nil
 }
@@ -72,4 +133,60 @@ func (r *ClientReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&wekav1alpha1.Client{}).
 		Owns(&appsv1.Deployment{}).
 		Complete(r)
+}
+
+func (r *ClientReconciler) deploymentForClient(client *wekav1alpha1.Client) (*appsv1.Deployment, error) {
+	ls := map[string]string{
+		"app.kubernetes.io": "weka-client",
+	}
+	replicas := int32(1)
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      client.Name,
+			Namespace: client.Namespace,
+			Labels:    ls,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: &[]bool{true}[0],
+						SeccompProfile: &corev1.SeccompProfile{
+							Type: corev1.SeccompProfileTypeRuntimeDefault,
+						},
+					},
+					Containers: []corev1.Container{{
+						Image:           "busybox:1.35",
+						Name:            "weka-client",
+						ImagePullPolicy: corev1.PullIfNotPresent,
+						SecurityContext: &corev1.SecurityContext{
+							RunAsNonRoot:             &[]bool{true}[0],
+							RunAsUser:                &[]int64{1000}[0],
+							AllowPrivilegeEscalation: &[]bool{false}[0],
+							Capabilities: &corev1.Capabilities{
+								Drop: []corev1.Capability{
+									"All",
+								},
+							},
+						},
+						Command: []string{"sleep", "3600"},
+					}},
+				},
+			},
+		},
+	}
+
+	dep.ObjectMeta.ResourceVersion = ""
+
+	// Set Client instance as the owner and controller
+	ctrl.SetControllerReference(client, dep, r.Scheme)
+	return dep, nil
 }
