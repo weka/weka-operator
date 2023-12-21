65b85f7cb9b65173874db400fcf5c3eb10aade92
Matthew Pfefferle <matthew.pfefferle@weka.io>
2023-12-21 14:56:42 -0500

    Verify wekafsio module created

--- controllers/client_controller_test.go
+++ controllers/client_controller_test.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"time"
+
+	"github.com/kubernetes-sigs/kernel-module-management/api/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	wekav1alpha1 "github.com/weka/weka-operator/api/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("Client controller", func() {
@@ -33,8 +37,8 @@ var _ = Describe("Client controller", func() {
 
 	Context("When creating a client", func() {
 		var clientCreated error
-		// timeout := time.Second * 10
-		// interval := time.Millisecond * 250
+		timeout := time.Second * 10
+		interval := time.Millisecond * 250
 
 		BeforeEach(func() {
 			// Clean up existing clients in case of failed runs
@@ -53,16 +57,16 @@ var _ = Describe("Client controller", func() {
 			Expect(clientCreated).To(Succeed())
 		})
 		It("should create a wekafsio module", func() {
-			//createdModule := &v1beta1.Module{}
-			//deployKey := types.NamespacedName{
-			//Name:      "wekafsio",
-			//Namespace: testNamespace,
-			//}
-			//Eventually(func() bool {
-			//err := k8sClient.Get(testCtx, deployKey, createdModule)
-			//return err == nil
-			//}, timeout, interval).Should(BeTrue())
-			//Expect(createdModule.Spec.ModuleLoader.Container.Modprobe.ModuleName).To(Equal("wekafsio"))
+			createdModule := &v1beta1.Module{}
+			deployKey := types.NamespacedName{
+				Name:      "wekafsio",
+				Namespace: testNamespace,
+			}
+			Eventually(func() bool {
+				err := k8sClient.Get(testCtx, deployKey, createdModule)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+			Expect(createdModule.Spec.ModuleLoader.Container.Modprobe.ModuleName).To(Equal("wekafsio"))
 		})
 	})
 })
