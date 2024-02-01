06b0e8edeef5a05330b4b94549071911ddf33bf8
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-02-01 14:39:44 -0500

    build: added terraform and ansible to build an eks cluster

---
+++ terraform/eks/main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_version = ">= 1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "devkube"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+  }
+}
+
+provider "kubectl" {}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+
+  registry {
+    url      = "oci://quay.io/weka.io/helm"
+    username = "matthew_pfefferle_weka"
+    password = "t/leZi8upBj5sxf9X9DY++HxEIU5D6XUQLr13m7dsDv9ArZhQmTuTrWPdsIsMGF3r5S03YqnK6Jd6LxANnv1dA=="
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
+
+locals {
+  prefix             = "weka"
+  cluster_name       = "mpfefferle"
+  kubernetes_version = "1.29"
+}
+
+
+resource "aws_vpc" "weka_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "weka_subnet1" {
+  vpc_id                  = aws_vpc.weka_vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                              = "weka_subnet1"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "weka_subnet2" {
+  vpc_id                  = aws_vpc.weka_vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                              = "weka_subnet2"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.weka_vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "weka_public_subnet"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.weka_igw]
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_internet_gateway" "weka_igw" {
+  vpc_id = aws_vpc.weka_vpc.id
+}
+
+resource "aws_route_table" "weka_route_table" {
+  vpc_id = aws_vpc.weka_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.weka_igw.id
+  }
+}
+
+resource "aws_route_table_association" "weka_subnet1_association" {
+  subnet_id      = aws_subnet.weka_subnet1.id
+  route_table_id = aws_route_table.weka_route_table.id
+}
+
+resource "aws_route_table_association" "weka_subnet2_association" {
+  subnet_id      = aws_subnet.weka_subnet2.id
+  route_table_id = aws_route_table.weka_route_table.id
+}
---
+++ terraform/eks/variables.tf
@@ -0,0 +1,4 @@
+variable "get_weka_io_token" {
+  description = "Weka.io token"
+  type        = string
+}
