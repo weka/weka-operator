9feb7ff0a847b73a676df92ff3e062f11146248f
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-02-23 17:45:46 -0500

    chore: move api types to pkg

--- internal/app/manager/controllers/api_key.go
+++ internal/app/manager/controllers/api_key.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 
 	"github.com/pkg/errors"
-	wekav1alpha1 "github.com/weka/weka-operator/internal/app/manager/api/v1alpha1"
+	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
 	v1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
