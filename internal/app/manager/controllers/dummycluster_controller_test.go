c8a6dc95f9238c5006649844344a88ab8056f85e
Anton Bykov <tigrawap@gmail.com>
2024-03-19 12:39:41 +0200

    test: disable all tests for now, until re-enabled one by one with definition

--- internal/app/manager/controllers/dummycluster_controller_test.go
+++ internal/app/manager/controllers/dummycluster_controller_test.go
@@ -17,67 +17,58 @@ limitations under the License.
 package controllers
 
 import (
-	"context"
-	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
-
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("DummyCluster Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
-
-		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		dummycluster := &wekav1alpha1.DummyCluster{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind DummyCluster")
-			err := k8sClient.Get(ctx, typeNamespacedName, dummycluster)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &wekav1alpha1.DummyCluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &wekav1alpha1.DummyCluster{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance DummyCluster")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &DummyClusterReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
+	//Context("When reconciling a resource", func() {
+	//	const resourceName = "test-resource"
+	//
+	//	ctx := context.Background()
+	//
+	//	typeNamespacedName := types.NamespacedName{
+	//		Name:      resourceName,
+	//		Namespace: "default", // TODO(user):Modify as needed
+	//	}
+	//	dummycluster := &wekav1alpha1.DummyCluster{}
+	//
+	//	BeforeEach(func() {
+	//		By("creating the custom resource for the Kind DummyCluster")
+	//		err := k8sClient.Get(ctx, typeNamespacedName, dummycluster)
+	//		if err != nil && errors.IsNotFound(err) {
+	//			resource := &wekav1alpha1.DummyCluster{
+	//				ObjectMeta: metav1.ObjectMeta{
+	//					Name:      resourceName,
+	//					Namespace: "default",
+	//				},
+	//				// TODO(user): Specify other spec details if needed.
+	//			}
+	//			Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+	//		}
+	//	})
+	//
+	//	AfterEach(func() {
+	//		// TODO(user): Cleanup logic after each test, like removing the resource instance.
+	//		resource := &wekav1alpha1.DummyCluster{}
+	//		err := k8sClient.Get(ctx, typeNamespacedName, resource)
+	//		Expect(err).NotTo(HaveOccurred())
+	//
+	//		By("Cleanup the specific resource instance DummyCluster")
+	//		Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+	//	})
+	//	It("should successfully reconcile the resource", func() {
+	//		By("Reconciling the created resource")
+	//		controllerReconciler := &DummyClusterReconciler{
+	//			Client: k8sClient,
+	//			Scheme: k8sClient.Scheme(),
+	//		}
+	//
+	//		_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+	//			NamespacedName: typeNamespacedName,
+	//		})
+	//		Expect(err).NotTo(HaveOccurred())
+	//		// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+	//		// Example: If you expect a certain status condition after reconciliation, verify it here.
+	//	})
+	//})
 })
