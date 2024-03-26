e9b0f9d78a2ebc1ca0676292b69cf83eef1aa6e8
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-03-26 13:51:33 -0400

    feat: get cluster rest api

--- internal/app/manager/rest_api/cluster_api.go
+++ internal/app/manager/rest_api/cluster_api.go
@@ -2,12 +2,16 @@ package restapi
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"net"
 	"net/http"
 
 	"github.com/go-logr/logr"
+	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func NewClusterAPI(mgr ctrl.Manager) *ClusterAPI {
@@ -15,7 +19,6 @@ func NewClusterAPI(mgr ctrl.Manager) *ClusterAPI {
 	mux := http.NewServeMux()
 	api := &ClusterAPI{
 		logger: mgr.GetLogger().WithName("ClusterAPI"),
-		ctx:    ctx,
 		cancel: cancel,
 		mux:    mux,
 		server: &http.Server{
@@ -25,6 +28,7 @@ func NewClusterAPI(mgr ctrl.Manager) *ClusterAPI {
 				return context.WithValue(ctx, "serverAddr", listener.Addr().String())
 			},
 		},
+		client: mgr.GetClient(),
 	}
 	api.registerRoutes()
 	return api
@@ -36,6 +40,7 @@ type ClusterAPI struct {
 	cancel context.CancelFunc
 	mux    *http.ServeMux
 	server *http.Server
+	client client.Client
 }
 
 func (api *ClusterAPI) StartServer(mgr ctrl.Manager) {
@@ -55,7 +60,45 @@ func (api *ClusterAPI) index(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api *ClusterAPI) getCluster(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, Cluster!"))
+	logger := api.logger.WithName("getCluster")
+
+	// Get the cluster object
+	ctx := r.Context()
+	name := r.URL.Query().Get("name")
+	namespace := r.URL.Query().Get("namespace")
+
+	if name == "" || namespace == "" {
+		logger.Error(errors.New("name and namespace are required"), "Name and namespace are required")
+		http.Error(w, "Name and namespace are required", http.StatusBadRequest)
+		return
+	}
+
+	cluster := &wekav1alpha1.DummyCluster{}
+	key := client.ObjectKey{Name: name, Namespace: namespace}
+	err := api.client.Get(ctx, key, cluster)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Error(err, "Cluster not found")
+			http.Error(w, "Cluster not found", http.StatusNotFound)
+			return
+		}
+		logger.Error(err, "Failed to get cluster")
+		http.Error(w, "Failed to get cluster", http.StatusInternalServerError)
+		return
+	}
+
+	// Marshal the cluster object to JSON
+	data, err := json.Marshal(cluster)
+	if err != nil {
+		logger.Error(err, "Failed to marshal cluster")
+		http.Error(w, "Failed to marshal cluster", http.StatusInternalServerError)
+		return
+	}
+
+	// Write the JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+	return
 }
 
 func (api *ClusterAPI) registerRoutes() {
