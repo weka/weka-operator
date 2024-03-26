df6d46f06fa1979507dd80baf7b55664f1165519
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-03-26 16:06:38 -0400

    feat: get cluster status

--- internal/app/manager/rest_api/cluster_api.go
+++ internal/app/manager/rest_api/cluster_api.go
@@ -62,8 +62,9 @@ func (api *ClusterAPI) getCluster(w rest.ResponseWriter, r *rest.Request) {
 
 	// Get the cluster object
 	ctx := r.Context()
-	name := r.URL.Query().Get("name")
-	namespace := r.URL.Query().Get("namespace")
+	name := r.PathParam("name")
+	namespace := r.PathParam("namespace")
+	logger.Info("Getting cluster", "name", name, "namespace", namespace)
 
 	if name == "" || namespace == "" {
 		err := pretty.Errorf("name and namespace are required")
@@ -92,10 +93,65 @@ func (api *ClusterAPI) getCluster(w rest.ResponseWriter, r *rest.Request) {
 	return
 }
 
+func (api *ClusterAPI) getClusterStatus(w rest.ResponseWriter, r *rest.Request) {
+	logger := api.logger.WithName("getClusterStatus")
+
+	ctx := r.Context()
+	name := r.PathParam("name")
+	namespace := r.PathParam("namespace")
+	logger.Info("Getting cluster status", "name", name, "namespace", namespace)
+
+	if name == "" || namespace == "" {
+		err := pretty.Errorf("name and namespace are required")
+		logger.Error(err, "Name and namespace are required")
+		rest.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	cluster := &wekav1alpha1.DummyCluster{}
+	key := client.ObjectKey{Name: name, Namespace: namespace}
+	err := api.client.Get(ctx, key, cluster)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Error(err, "Cluster not found")
+			rest.Error(w, "Cluster not found", http.StatusNotFound)
+			return
+		}
+		logger.Error(err, "Failed to get cluster")
+		rest.Error(w, "Failed to get cluster", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteJson(cluster.Status)
+}
+
+func (api *ClusterAPI) listClusters(w rest.ResponseWriter, r *rest.Request) {
+	logger := api.logger.WithName("listClusters")
+	logger.Info("Listing clusters")
+
+	// List all clusters
+	ctx := r.Context()
+	clusters := &wekav1alpha1.DummyClusterList{}
+	err := api.client.List(ctx, clusters)
+	if err != nil {
+		logger.Error(err, "Failed to list clusters")
+		rest.Error(w, "Failed to list clusters", http.StatusInternalServerError)
+		return
+	}
+
+	// Write the JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteJson(clusters)
+	return
+}
+
 func (api *ClusterAPI) registerRoutes() {
 	router, err := rest.MakeRouter(
 		rest.Get("/", api.index),
-		rest.Get("/cluster", api.getCluster),
+		rest.Get("/clusters/:namespace/:name", api.getCluster),
+		rest.Get("/clusters/:namespace/:name/status", api.getClusterStatus),
+		rest.Get("/clusters", api.listClusters),
 	)
 	if err != nil {
 		api.logger.Error(err, "Failed to create router")
