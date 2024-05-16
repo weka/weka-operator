38e2cea82eb8fc4a65ac4d6534743764f11a006d
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-16 15:26:09 -0400

    refactor: getorinitallocmap

--- internal/app/manager/services/crd.go
+++ internal/app/manager/services/crd.go
@@ -3,16 +3,22 @@ package services
 import (
 	"context"
 
+	"github.com/weka/weka-operator/internal/app/manager/domain"
 	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
 	"github.com/weka/weka-operator/internal/pkg/instrumentation"
+	"github.com/weka/weka-operator/util"
+	"gopkg.in/yaml.v2"
 
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type CrdManager interface {
 	GetCluster(ctx context.Context, req ctrl.Request) (WekaClusterService, error)
+	GetOrInitAllocMap(ctx context.Context) (*domain.Allocations, *v1.ConfigMap, error)
 }
 
 func NewCrdManager(mgr ctrl.Manager) CrdManager {
@@ -41,6 +47,55 @@ func (r *crdManager) GetCluster(ctx context.Context, req ctrl.Request) (WekaClus
 	wekaClusterService := NewWekaClusterService(r.Manager, wekaCluster)
 
 	return wekaClusterService, err
+}
+
+func (r *crdManager) GetOrInitAllocMap(ctx context.Context) (*domain.Allocations, *v1.ConfigMap, error) {
+	ctx, logger, end := instrumentation.GetLogSpan(ctx, "GetOrInitAllocMap")
+	defer end()
+	// fetch alloc map from configmap
+	allocations := &domain.Allocations{
+		NodeMap: domain.AllocationsMap{},
+	}
+	allocMap := allocations.NodeMap
+	yamlData, err := yaml.Marshal(&allocMap)
+	if err != nil {
+		logger.Error(err, "Failed to marshal alloc map")
+		return nil, nil, err
+	}
+
+	allocMapConfigMap := &v1.ConfigMap{}
+	podNamespace, err := util.GetPodNamespace()
+	if err != nil {
+		logger.Error(err, "Failed to get pod namespace")
+		return nil, nil, err
+	}
+	key := client.ObjectKey{Namespace: podNamespace, Name: "weka-operator-allocmap"}
+	err = r.getClient().Get(ctx, key, allocMapConfigMap)
+	if err != nil && apierrors.IsNotFound(err) {
+		// Define a new ConfigMap
+		allocMapConfigMap = &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "weka-operator-allocmap",
+				Namespace: podNamespace,
+			},
+			Data: map[string]string{
+				"allocmap.yaml": string(yamlData),
+			},
+		}
+		err = r.getClient().Create(ctx, allocMapConfigMap)
+		if err != nil {
+			return nil, nil, err
+		}
+	} else {
+		if err != nil {
+			return nil, nil, err
+		}
+		err = yaml.Unmarshal([]byte(allocMapConfigMap.Data["allocmap.yaml"]), &allocations)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	return allocations, allocMapConfigMap, nil
 }
 
 func (r *crdManager) getClient() client.Client {
--- internal/app/manager/services/crd_test.go
+++ internal/app/manager/services/crd_test.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
 
 	"github.com/pkg/errors"
 	"go.uber.org/mock/gomock"
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
@@ -75,6 +77,65 @@ func TestGetCluster(t *testing.T) {
 			if (tt.cluster == nil && cluster != nil) || (tt.cluster != nil && cluster == nil) {
 				t.Errorf("Expected %v, got %v", tt.cluster, cluster)
 			}
+		})
+	}
+}
+
+func TestGetOrInitAllocMap(t *testing.T) {
+	fixtures := setup(t)
+	defer fixtures.teardown()
+
+	os.Setenv("OPERATOR_DEV_MODE", "true")
+
+	config := &rest.Config{}
+
+	fixtures.mockManager.EXPECT().GetClient().Return(fixtures.mockClient).AnyTimes()
+	fixtures.mockManager.EXPECT().GetConfig().Return(config).AnyTimes()
+
+	subject := &crdManager{
+		Manager: fixtures.mockManager,
+	}
+
+	ctx := context.Background()
+
+	tests := []struct {
+		name              string
+		apiError          error
+		err               error
+		validateConfigMap func(configMap *v1.ConfigMap)
+	}{
+		{
+			name:     "configmap not found - and create succeeds",
+			apiError: apierrors.NewNotFound(schema.GroupResource{}, "test-name"),
+			err:      nil,
+			validateConfigMap: func(configMap *v1.ConfigMap) {
+				if configMap.Name != "weka-operator-allocmap" {
+					t.Errorf("Expected weka-operator-allocmap, got %v", configMap.Name)
+				}
+			},
+		},
+		{
+			name:     "configmap found",
+			apiError: nil,
+			err:      nil,
+			validateConfigMap: func(configMap *v1.ConfigMap) {
+				if configMap.Name != "weka-operator-allocmap" {
+					t.Errorf("Expected weka-operator-allocmap, got %v", configMap.Name)
+				}
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fixtures.mockClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(tt.apiError).AnyTimes()
+			fixtures.mockClient.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+			_, configMap, err := subject.GetOrInitAllocMap(ctx)
+			if err != tt.err {
+				t.Errorf("Expected %v, got %v", tt.err, err)
+			}
+			tt.validateConfigMap(configMap)
 		})
 	}
 }
