16139ca1185b44f2b06b4acbf6fc432cb469229b
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-24 08:58:59 -0400

    refactor: cluster created condition

---
+++ internal/app/manager/controllers/lifecycle/containers_joined_cluster.go
@@ -0,0 +1,79 @@
+package lifecycle
+
+import (
+	"context"
+	"time"
+
+	"github.com/weka/weka-operator/internal/app/manager/controllers/condition"
+	"github.com/weka/weka-operator/internal/app/manager/services"
+	"github.com/weka/weka-operator/internal/pkg/errors"
+	"github.com/weka/weka-operator/internal/pkg/instrumentation"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type ContainerJoinError struct {
+	Err error
+}
+
+func (e ContainerJoinError) Error() string {
+	return "error joining containers to cluster: " + e.Err.Error()
+}
+
+func ContainersJoinedCluster(wekaClusterService services.WekaClusterService, statusClient StatusClient) StepFunc {
+	return func(ctx context.Context, state *ReconciliationState) error {
+		ctx, logger, end := instrumentation.GetLogSpan(ctx, "ContainersJoinedCluster")
+		defer end()
+
+		if state.Cluster == nil {
+			return &errors.ArgumentError{ArgName: "Cluster", Message: "Cluster is nil"}
+		}
+		wekaCluster := state.Cluster
+
+		if state.Containers == nil {
+			return &errors.ArgumentError{ArgName: "Containers", Message: "Containers is nil"}
+		}
+		if len(*state.Containers) == 0 {
+			return &errors.ArgumentError{ArgName: "Containers", Message: "Containers is empty"}
+		}
+		containers := *state.Containers
+
+		logger.Debug("Ensuring all containers are up in the cluster")
+		joinedContainers := 0
+		for _, container := range containers {
+			if !meta.IsStatusConditionTrue(container.Status.Conditions, condition.CondJoinedCluster) {
+				logger.Info("Container has not joined the cluster yet", "container", container.Name)
+				logger.SetPhase("CONTAINERS_NOT_JOINED_CLUSTER")
+
+				statusClient.SetCondition(ctx, wekaCluster, condition.CondJoinedCluster, metav1.ConditionFalse, "Init", "Containers have not joined the cluster yet")
+
+				return &RetryableError{Err: nil, RetryAfter: time.Second * 3}
+			} else {
+				if wekaCluster.Status.ClusterID == "" {
+					wekaCluster.Status.ClusterID = container.Status.ClusterID
+					if err := statusClient.UpdateStatus(ctx, wekaCluster); err != nil {
+						return &ContainerJoinError{Err: err}
+					}
+					logger.Info("Container joined cluster successfully", "container_name", container.Name)
+				}
+				joinedContainers++
+			}
+		}
+		if joinedContainers == len(containers) {
+			logger.SetPhase("ALL_CONTAINERS_ALREADY_JOINED")
+		} else {
+			logger.SetPhase("CONTAINERS_JOINED_CLUSTER")
+		}
+
+		if err := wekaClusterService.EnsureClusterContainerIds(ctx, containers); err != nil {
+			return &RetryableError{
+				Err:        &ContainerJoinError{Err: err},
+				RetryAfter: time.Second * 3,
+			}
+		}
+
+		statusClient.SetCondition(ctx, wekaCluster, condition.CondJoinedCluster, metav1.ConditionTrue, "Init", "All containers have joined the cluster")
+
+		return nil
+	}
+}
