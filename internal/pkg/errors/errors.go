ae433e4052d77389ca5f1847ca1bfecbd56e154e
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-24 14:55:58 -0400

    refactor(services): remove exec service from weka cluster service

--- internal/pkg/errors/errors.go
+++ internal/pkg/errors/errors.go
@@ -10,3 +10,15 @@ type ArgumentError struct {
 func (e ArgumentError) Error() string {
 	return fmt.Sprintf("argument error: %s: %s", e.ArgName, e.Message)
 }
+
+type WrappedError struct {
+	Err error
+}
+
+func (e WrappedError) Error() string {
+	return fmt.Sprintf("wrapped error: %v", e.Err)
+}
+
+func (e WrappedError) Unwrap() error {
+	return e.Err
+}
