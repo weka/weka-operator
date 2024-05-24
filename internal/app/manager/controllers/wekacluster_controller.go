c16ec425c5b63d71700b4eedd993aad86c004a6b
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-24 08:58:59 -0400

    refactor: cluster created condition

--- internal/app/manager/controllers/wekacluster_controller.go
+++ internal/app/manager/controllers/wekacluster_controller.go
@@ -251,38 +251,6 @@ func (r *WekaClusterReconciler) Reconcile(initContext context.Context, req ctrl.
 	}
 
 	// Ensure all containers are up in the cluster
-	logger.Debug("Ensuring all containers are up in the cluster")
-	joinedContainers := 0
-	for _, container := range containers {
-		if !meta.IsStatusConditionTrue(container.Status.Conditions, condition.CondJoinedCluster) {
-			logger.Info("Container has not joined the cluster yet", "container", container.Name)
-			logger.SetPhase("CONTAINERS_NOT_JOINED_CLUSTER")
-			return ctrl.Result{Requeue: true, RequeueAfter: time.Second * 3}, nil
-		} else {
-			if wekaCluster.Status.ClusterID == "" {
-				wekaCluster.Status.ClusterID = container.Status.ClusterID
-				err := r.Status().Update(ctx, wekaCluster)
-				if err != nil {
-					return ctrl.Result{}, err
-				}
-				logger.Info("Container joined cluster successfully", "container_name", container.Name)
-			}
-			joinedContainers++
-		}
-	}
-	if joinedContainers == len(containers) {
-		logger.SetPhase("ALL_CONTAINERS_ALREADY_JOINED")
-	} else {
-		logger.SetPhase("CONTAINERS_JOINED_CLUSTER")
-	}
-
-	err = wekaClusterService.EnsureClusterContainerIds(ctx, containers)
-	if err != nil {
-		logger.Info("not all containers are up in the cluster", "err", err)
-		return ctrl.Result{Requeue: true, RequeueAfter: time.Second}, nil
-	}
-
-	// Ensure all containers are up in the cluster
 	logger.Debug("Ensuring all drives are up in the cluster")
 	for _, container := range containers {
 		if container.Spec.Mode != "drive" {
