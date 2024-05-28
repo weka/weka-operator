3408f08a4b021b910623559162718c4717c4c8cc
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-28 13:12:24 -0400

    refactor: cond client secrets created

---
+++ internal/app/manager/controllers/lifecycle/cluster_client_secrets_created.go
@@ -0,0 +1,38 @@
+package lifecycle
+
+import (
+	"context"
+
+	"github.com/weka/weka-operator/internal/app/manager/services"
+	"github.com/weka/weka-operator/internal/pkg/errors"
+	"github.com/weka/weka-operator/internal/pkg/instrumentation"
+)
+
+type ClientSecretCreationError struct {
+	errors.WrappedError
+}
+
+func (state *ClusterState) ClusterClientSecretsCreated(secretsService services.SecretsService) StepFunc {
+	return func(ctx context.Context) error {
+		ctx, _, end := instrumentation.GetLogSpan(ctx, "ClusterClientSecretsCreated")
+		defer end()
+
+		if state.Subject == nil {
+			return &errors.ArgumentError{ArgName: "Cluster", Message: "Cluster is nil"}
+		}
+		wekaCluster := state.Subject
+
+		if state.Containers == nil {
+			return &errors.ArgumentError{ArgName: "Containers", Message: "Containers is nil"}
+		}
+		containers := state.Containers
+		if len(containers) == 0 {
+			return &errors.ArgumentError{ArgName: "Containers", Message: "Containers is empty"}
+		}
+
+		if err := secretsService.EnsureClientLoginCredentials(ctx, wekaCluster, containers); err != nil {
+			return &ClientSecretCreationError{}
+		}
+		return nil
+	}
+}
