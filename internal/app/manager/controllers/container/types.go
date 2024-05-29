9a35939df5af800f40964eae4a75b3a1cdb6683b
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-05-29 11:05:45 -0400

    refactor: ensure finalizer

--- internal/app/manager/controllers/container/types.go
+++ internal/app/manager/controllers/container/types.go
@@ -3,8 +3,14 @@ package container
 import (
 	"github.com/weka/weka-operator/internal/app/manager/controllers/lifecycle"
 	wekav1alpha1 "github.com/weka/weka-operator/internal/pkg/api/v1alpha1"
+	"github.com/weka/weka-operator/internal/pkg/errors"
 )
 
 type ContainerState struct {
 	lifecycle.ReconciliationState[*wekav1alpha1.WekaContainer]
+}
+
+type ContainerUpdateError struct {
+	errors.WrappedError
+	Name string
 }
