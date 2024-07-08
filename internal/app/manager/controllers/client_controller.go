21f08fe73d3f2aa8343434e2a597873652d893c9
Sergey Berezansky <sergey@weka.io>
2024-07-08 15:44:59 +0300

    fix: redundant wrapping of EnsureNodeDiscovered

--- internal/app/manager/controllers/client_controller.go
+++ internal/app/manager/controllers/client_controller.go
@@ -125,15 +125,11 @@ func (r *ClientReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		wg.Add(1)
 		go func(node string) {
 			err := func(node string) error {
-				_, err2 := services.GetNodeDiscovery(ctx, r.Client, node) // just to check whether it exists and is up–to-date
-				if err2 != nil {
-					return services.EnsureNodeDiscovered(ctx, r.Client, services.OwnerWekaObject{
-						Image:           wekaClient.Spec.Image,
-						ImagePullSecret: wekaClient.Spec.ImagePullSecret,
-						Tolerations:     tolerations,
-					}, node, r.ExecService)
-				}
-				return nil
+				return services.EnsureNodeDiscovered(ctx, r.Client, services.OwnerWekaObject{
+					Image:           wekaClient.Spec.Image,
+					ImagePullSecret: wekaClient.Spec.ImagePullSecret,
+					Tolerations:     tolerations,
+				}, node, r.ExecService)
 			}(node)
 			if err != nil {
 				errs <- err
