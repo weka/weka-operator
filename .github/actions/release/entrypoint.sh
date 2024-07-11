6da966f6eac3adb8c31a09dff01e753acf67215c
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-07-11 10:19:42 -0400

    ci: compute next version from branch
    - Use `git-cliff` to compute version increment from conventional commits
    - Use `semver` to determine type of change

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -10,15 +10,24 @@ export GITHUB_TOKEN
 
 BRANCH="${2}"
 
-SEMANTIC_RELEASE=/usr/local/bin/semantic-release
-echo "Go Semantic Release: $(${SEMANTIC_RELEASE} --version)"
+GIT_CLIFF=/usr/local/bin/git-cliff
+echo "Git Cliff: $(${GIT_CLIFF} --version)"
+
+SEMVER=/usr/local/bin/semver
+echo "Semver: $(${SEMVER} --version)"
 
 set -x
 
-"${SEMANTIC_RELEASE}" --token "${GITHUB_TOKEN}" \
-  --provider-opt slug=weka/weka-operator \
-  --hooks goreleaser \
-  --version-file \
-  --dry \
-  --prerelease \
-  --match "$(jq -r '.match' <.semrelrc)"
+whoami
+ls -al .
+
+${GIT_CLIFF} --unreleased
+${GIT_CLIFF} --unreleased --bumped-version
+
+CURRENT_VERSION=$(git describe --tags --abbrev=0)
+NEXT_VERSION=$(${GIT_CLIFF} --unreleased --bumped-version)
+DIFF=$(semver diff "${CURRENT_VERSION}" "${NEXT_VERSION}")
+
+echo "Current Version: ${CURRENT_VERSION}"
+echo "Next Version: ${NEXT_VERSION}"
+echo "Diff: ${DIFF}"
