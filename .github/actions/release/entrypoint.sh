94e3e48584726e14f608e58d4c6887fed83395c0
Matthew Pfefferle <matthew.pfefferle@weka.io>
2024-07-11 13:35:39 -0400

    ci: pre-release rules
    - Pull requests don't generate a release, unless they have the release
      label
    - Release branches create release builds and beta branches create
      pre-release builds

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -9,6 +9,7 @@ GITHUB_TOKEN="${1}"
 export GITHUB_TOKEN
 
 BRANCH="${2}"
+EVENT="${3}"
 
 GIT_CLIFF=/usr/local/bin/git-cliff
 echo "Git Cliff: $(${GIT_CLIFF} --version)"
@@ -16,10 +17,8 @@ echo "Git Cliff: $(${GIT_CLIFF} --version)"
 SEMVER=/usr/local/bin/semver
 echo "Semver: $(${SEMVER} --version)"
 
-set -x
-
-whoami
-ls -al .
+GH=/usr/local/bin/gh
+echo "GH: $(${GH} --version)"
 
 ${GIT_CLIFF} --unreleased
 ${GIT_CLIFF} --unreleased --bumped-version
@@ -29,5 +28,54 @@ NEXT_VERSION=$(${GIT_CLIFF} --unreleased --bumped-version)
 DIFF=$(semver diff "${CURRENT_VERSION}" "${NEXT_VERSION}")
 
 echo "Current Version: ${CURRENT_VERSION}"
-echo "Next Version: ${NEXT_VERSION}"
-echo "Diff: ${DIFF}"
+
+# If the branch is main, skip releasing for now
+if [ "${BRANCH}" == "main" ]; then
+  echo "Main branch, skipping release"
+  exit 0
+fi
+
+# If the branch is a beta branch, release
+# Betas are released from the release/v1-beta branch
+if [[ "${BRANCH}" =~ ^release/v1-beta$ ]]; then
+  echo "Beta branch, releasing"
+  # Bump prerelease version
+  NEXT_VERSION=$(semver bump prerelease "${NEXT_VERSION}")
+
+  # Create a release
+  # This is done as a prerelease for now it'll get promoted to a full release
+  # once the binaries are built and published
+  ${GH} release create "${NEXT_VERSION}" --title "${NEXT_VERSION}" --notes "Release ${NEXT_VERSION}" --prerelease
+  exit 0
+fi
+
+# If the branch is a release branch, release
+if [[ "${BRANCH}" =~ ^release/.*$ ]]; then
+  echo "Release branch, releasing"
+  # Bump release version, removing the prerelease qualifiers if present
+  NEXT_VERSION=$(semver bump release "${NEXT_VERSION}")
+
+  # Create a release
+  # This is done as a prerelease for now it'll get promoted to a full release
+  # once the binaries are built and published
+  ${GH} release create "${NEXT_VERSION}" --title "${NEXT_VERSION}" --notes "Release ${NEXT_VERSION}" --prerelease
+  exit 0
+fi
+
+# If the branch is is part of a PR, look for a release label
+# If the PR has a release label, release, otherwise skip
+if gh pr view --json 'id' >/dev/null; then
+  echo "PR found"
+  PR_LABELS=$(gh pr view --json labels --jq '.labels[].name')
+  echo "PR Labels: ${PR_LABELS}"
+  if ! echo "${PR_LABELS}" | grep -q "release"; then
+    echo "Release label not found"
+    exit 0
+  fi
+
+  echo "Release label found"
+  # Chop refs/heads/ from the branch name
+  BRANCH=${BRANCH#refs/heads/}
+  NEXT_VERSION=$(semver bump build "${BRANCH//[^[:alnum:]]/.}" "${NEXT_VERSION}")
+  ${GH} release create "${NEXT_VERSION}" --title "${NEXT_VERSION}" --notes "Release ${NEXT_VERSION}" --prerelease
+fi
