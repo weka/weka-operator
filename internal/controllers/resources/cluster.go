1e4fa52f122697800c34fd01cd9116d10b8d22f3
Anton Bykov <tigrawap@gmail.com>
2024-08-27 16:09:05 +0300

    refactor: remove dead code

--- internal/controllers/resources/cluster.go
+++ internal/controllers/resources/cluster.go
@@ -1,7 +1,6 @@
 package resources
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -23,40 +22,6 @@ func (c ClusterContainer) ContainerId() (int, error) {
 type ClusterContainersResponse []ClusterContainer
 
 type ClusterContainersMap map[string]ClusterContainer
-
-func MapByContainerName(containers ClusterContainersResponse) (ClusterContainersMap, error) {
-	result := ClusterContainersMap{}
-	for _, container := range containers {
-		if ok := result[container.ContainerName]; ok.ContainerName != "" {
-			// HACK: If already exists, but is dead - skipping it,
-			// we would not reach this flow if we would not already delete previous on operator side
-			// Proper solution would be to bind WekaContainer with actual system Container on deletion
-			// But then need to have special handling of full deletion process, next iteration
-			// Also, nothing guarantees order here... It is bizzare we cannot find container ID locally
-			if ok.Status == "DOWN" {
-				if container.Status != "DOWN" {
-					result[container.ContainerName] = container
-					goto INSERT
-				}
-			}
-			if container.Status == "DOWN" {
-				if ok.Status != "DOWN" {
-					continue
-				}
-			}
-
-			if container.Status == "DOWN" {
-				if ok.Status == "DOWN" {
-					goto INSERT
-				}
-			}
-			return nil, fmt.Errorf("duplicate container name: %s", container.ContainerName)
-		}
-	INSERT:
-		result[container.ContainerName] = container
-	}
-	return result, nil
-}
 
 func HostIdToContainerId(hostId string) (int, error) {
 	hostId = strings.Replace(hostId, "HostId<", "", 1)
