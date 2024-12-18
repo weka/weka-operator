385ddbec3508d933c7e43045401ba9219afc5ace
Dan Mordechay <dan.mordechay@weka.io>
2024-12-18 14:29:24 +0200

    new taskmon version

--- build/taskmon/manual_buildx_push.sh
+++ build/taskmon/manual_buildx_push.sh
@@ -1,15 +1,12 @@
 #!/bin/zsh
 
-
 set -e
 
-BASE_IMAGE=6d3eb658e900b4d9_x86_64
-VERSION=0.0.9
+BASE_IMAGE=cc21256cddb833ab_x86_64
+VERSION=0.0.15
 
 TARGET_IMAGE=quay.io/weka.io/taskmon:$VERSION-$BASE_IMAGE
 
-
 docker buildx build --build-arg BASE_IMAGE=$BASE_IMAGE --push --platform linux/amd64 -t $TARGET_IMAGE .
-
 
 echo $TARGET_IMAGE is built
