11546f1f94aeadb931916936501afc4bb48ae65c
Anton Bykov <tigrawap@gmail.com>
2025-02-02 12:53:34 +0200

    fix: accidentally preserved short name used for debugging

--- internal/controllers/factory/container_factory.go
+++ internal/controllers/factory/container_factory.go
@@ -111,7 +111,7 @@ func NewWekaContainerForWekaCluster(cluster *wekav1alpha1.WekaCluster,
 		Spec: wekav1alpha1.WekaContainerSpec{
 			Image:                 cluster.Spec.Image,
 			ImagePullSecret:       cluster.Spec.ImagePullSecret,
-			WekaContainerName:     strings.Replace(name, "-", "x", -1)[:15],
+			WekaContainerName:     strings.Replace(name, "-", "x", -1),
 			Mode:                  role,
 			NumCores:              numCores,
 			ExtraCores:            extraCores,
