22df4f4f1cddaa205afcde654545f6c764052659
Anton Bykov <tigrawap@gmail.com>
2025-04-28 10:59:10 +0300

    chore: cleanup of dagger module

--- .dagger/src/operator_flows/main.py
+++ .dagger/src/operator_flows/main.py
@@ -11,41 +11,6 @@ from utils.github import GitHubClient
 @object_type
 class OperatorFlows:
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
-
-    @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
-        )
-
-    @function
-    async def gh_container(self, token: dagger.Secret) -> dagger.Container:
-        return await (
-            dag.container()
-            .from_("ghcr.io/astral-sh/uv:debian")
-            .with_exec(["bash", "-ec", """
-            (type -p wget >/dev/null || (apt update && apt-get install wget -y)) \
-            && mkdir -p -m 755 /etc/apt/keyrings \
-                && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-                && cat $out | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-            && chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-            && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-            && apt update \
-            && apt install gh -y
-            """])
-            .with_secret_variable("GH_TOKEN", token)
-        )
-
-    @function
     async def _get_pr_content(
             self,
             pr_number: int,
@@ -63,75 +28,6 @@ class OperatorFlows:
         Diff: <diff>{pr_diff}</diff>
         """
 
-    @function
-    async def ai_pr_release_notes(self,
-                                  pr_number: int,
-                                  repository: str,
-                                  gh_token: dagger.Secret
-                                  ) -> str:
-        body = await self._get_pr_content(
-            pr_number=pr_number,
-            repository=repository,
-            gh_token=gh_token
-        )
-        return await (
-            dag.llm()
-            .with_model("gemini-2.5-flash-preview-04-17")
-            .with_prompt(
-                f"""
-                You are analyzing a pull request description to determine if it is user-facing or not.
-                Return "Ignore" if not user facing
-                Return user-facing release notes if user-facing
-                PR:
-                
-                <body>{body}</body>
-
-                """
-            ).last_reply()
-        )
-
-    @function
-    async def ai_build_test_plan(self,
-                                 pr_numbers: List[int],
-                                 repository: str,
-                                 gh_token: dagger.Secret
-                                 ) -> str:
-        bodies = []
-        for pr_number in pr_numbers:
-            body = await self._get_pr_content(
-                pr_number=pr_number,
-                repository=repository,
-                gh_token=gh_token
-            )
-            bodies.append(body)
-
-        bodies_str = "\n".join(f"<body>{body}</body>" for body in bodies)
-
-        return await (
-            dag.llm()
-            .with_model("gemini-2.5-flash-preview-04-17")
-            .with_prompt(
-                f"""
-                Produce a test plan for the following pull requests.
-                Organize it as a clear step, optimizing for covering multiple pull requests at once
-                Respond with complete plan in markdown format
-                
-                PRs:
-                {bodies_str}
-                
-
-                """
-            ).last_reply()
-        )
-
-    @function
-    async def publish_dummy(self) -> str:
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_exec(["echo", "dummy"])
-            .publish("images.scalar.dev.weka.io:5002/alpine:latest")
-        )
 
     @function
     async def ci_on_merge_queue_env(self,
@@ -153,8 +49,6 @@ class OperatorFlows:
                                         ".git",
                                     ])],
                                     sock: dagger.Socket,
-                                    kubeconfig_path: dagger.Secret,
-                                    operator_version: str = "",
                                     ) -> dagger.Container:
         from containers.builders import _uv_base
         from apps.operator import publish_operator, publish_operator_helm_chart
@@ -163,10 +57,10 @@ class OperatorFlows:
         testing = await build_go(testing, sock, cache_deps=False)
         operator_image = await publish_operator(operator, sock,
                                                 repository="images.scalar.dev.weka.io:5002/weka-operator",
-                                                version=operator_version)
+                                                )
         operator_helm = await publish_operator_helm_chart(operator, sock,
                                                           repository="images.scalar.dev.weka.io:5002/helm",
-                                                          version=operator_version)
+                                                          )
 
         base_container = await _uv_base()
         base_container = (
@@ -175,14 +69,6 @@ class OperatorFlows:
             .with_file("/weka-k8s-testing", testing.file("/out-binary"))
             .with_new_file("/versions", f"{operator_image}\n{operator_helm}")
         )
-
-        if kubeconfig_path:
-            # Mount local kubeconfig file
-            base_container = (
-                base_container
-                .with_exec(["mkdir", "-p", "/.kube"])
-                .with_mounted_secret("/.kube/config", kubeconfig_path)
-            )
 
         return base_container
 
@@ -240,13 +126,19 @@ class OperatorFlows:
                                      openai_api_key: dagger.Secret,
                                      gemini_api_key: dagger.Secret,
                                      kubeconfig_path: dagger.Secret,
-                                     operator_version: str = "",
                                      initial_weka_version: str = "quay.io/weka.io/weka-in-container:4.4.5.95-k8s-safe-stop-and-metrics-alpha",
                                      new_weka_version: str = "quay.io/weka.io/weka-in-container:4.4.5.118-k8s.3",
-                                     ) -> str:
-        env = await self.ci_on_merge_queue_env(operator, testing, wekai, sock, kubeconfig_path, operator_version)
+                                     dry_run: bool = False,
+                                     ) -> dagger.Container:
 
-        # TODO: we will execute(?) upgade test in this env, but should it be part of dagger flow? or only prepare images and so?
+        env = await self.ci_on_merge_queue_env(operator, testing, wekai, sock)
+
+
+        env = (
+            env
+            .with_exec(["mkdir", "-p", "/.kube"])
+            .with_mounted_secret("/.kube/config", kubeconfig_path)
+        )
 
         gh_client = GitHubClient("weka/weka-operator", await gh_token.plaintext())
         pr = gh_client.get_pr_details(mq_pr_number)
@@ -313,25 +205,26 @@ class OperatorFlows:
         operator_image = operator_image_with_version.removesuffix(f":{operator_version}")
 
         # Execute the upgrade test command
-        result = await (
-            upgrade_test_container
-            .with_exec([
-                "/weka-k8s-testing",
-                "upgrade-extended",
-                "--initial-version", initial_weka_version,
-                "--new-version", new_weka_version,
-                "--operator-version", operator_version,
-                "--operator-image", operator_image,
-                "--operator-helm-image", operator_helm_image,
-                "--node-selector", "weka.io/dedicated:upgrade-extended",
-                "--namespace", "test-upgrade-extended",
-                "--cluster-name", "upgrade-extended",
-                # "--cleanup"
-            ])
-            .stdout()
-        )
-
-        return result
+        if not dry_run:
+            result = await (
+                upgrade_test_container
+                .with_exec([
+                    "/weka-k8s-testing",
+                    "upgrade-extended",
+                    "--initial-version", initial_weka_version,
+                    "--new-version", new_weka_version,
+                    "--operator-version", operator_version,
+                    "--operator-image", operator_image,
+                    "--operator-helm-image", operator_helm_image,
+                    "--node-selector", "weka.io/dedicated:upgrade-extended",
+                    "--namespace", "test-upgrade-extended",
+                    "--cluster-name", "upgrade-extended",
+                    # "--cleanup-on-start"
+                ])
+            )
+            return result
+        else:
+            return await upgrade_test_container
 
     @function
     async def generate_ai_plan_for_prs(
