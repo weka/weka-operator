85b6cb99ebd795b99ff374fcedd8e4c5003ad0f9
Assaf Giladi <assaf@weka.io>
2025-07-04 16:28:42 +0300

    fix: csi: support skipping controller creation

--- internal/config/env.go
+++ internal/config/env.go
@@ -129,6 +129,7 @@ var Config struct {
 
 	CsiInstallationEnabled          bool
 	CsiStorageClassCreationDisabled bool
+	CsiControllerCreationDisabled   bool
 	CsiImage                        string
 	CsiProvisionerImage             string
 	CsiAttacherImage                string
@@ -287,6 +288,7 @@ func ConfigureEnv(ctx context.Context) {
 	// CSI configuration
 	Config.CsiInstallationEnabled = getBoolEnvOrDefault("CSI_INSTALLATION_ENABLED", false)
 	Config.CsiStorageClassCreationDisabled = getBoolEnvOrDefault("CSI_STORAGE_CLASS_CREATION_DISABLED", false)
+	Config.CsiControllerCreationDisabled = getBoolEnvOrDefault("CSI_CONTROLLER_CREATION_DISABLED", false)
 	Config.CsiImage = env.GetString("CSI_IMAGE", "")
 	Config.CsiProvisionerImage = env.GetString("CSI_PROVISIONER_IMAGE", "")
 	Config.CsiAttacherImage = env.GetString("CSI_ATTACHER_IMAGE", "")
