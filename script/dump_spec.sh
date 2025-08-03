04013e92c6bc45a358282375b153e31a0a5d469f
Assaf Giladi <assaf@weka.io>
2025-08-03 10:15:30 +0300

    docs: update spec dump

--- script/dump_spec.sh
+++ script/dump_spec.sh
@@ -3,8 +3,12 @@
 # should be ran from directory that should get spec dumped into
 # k8s cluster should be set in kubeconfig
 
+cd doc/spec_dump || (echo "dump repo isn't found" && exit 1)
+
 kubectl explain --recursive wekacontainer > wekacontainer.spec.txt
 kubectl explain --recursive wekacluster > wekacluster.spec.txt
 kubectl explain --recursive wekaclient > wekaclient.spec.txt
 kubectl explain --recursive wekapolicy > wekapolicy.spec.txt
 kubectl explain --recursive wekamanualoperation > wekamanualoperation.spec.txt
+
+echo "specs dumped to doc/spec_dump"
