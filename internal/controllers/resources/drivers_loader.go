cacd9a7186a61e606820e3785233d2ec83c74997
Yaron Bar <yaron.bar@weka.io>
2025-08-19 13:14:46 +0300

    fix: add uio init container for COS

--- internal/controllers/resources/drivers_loader.go
+++ internal/controllers/resources/drivers_loader.go
@@ -54,3 +54,70 @@ func (f *PodFactory) copyWekaVersionToDriverLoader(pod *corev1.Pod) {
 		},
 	})
 }
+
+func (f *PodFactory) addUIOLoaderInitContainer(pod *corev1.Pod) *corev1.Pod {
+	if pod == nil {
+		return nil
+	}
+
+	script := `#!/bin/sh
+set -e
+
+echo "Loading UIO kernel module..."
+modprobe uio
+lsmod | grep uio
+echo "UIO module loaded successfully"
+`
+
+	command := []string{"/bin/sh", "-c", script}
+	privileged := true
+	hostPathType := corev1.HostPathUnset
+
+	uioInitContainer := corev1.Container{
+		Name:    "uio-loader-init",
+		Image:   "busybox:latest",
+		Command: command,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: &privileged,
+		},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "host-modules",
+				MountPath: "/lib/modules",
+				ReadOnly:  true,
+			},
+		},
+	}
+
+	if pod.Spec.InitContainers == nil {
+		pod.Spec.InitContainers = []corev1.Container{}
+	}
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers, uioInitContainer)
+
+	// Add the required volume if it doesn't already exist
+	volumeExists := false
+	for _, vol := range pod.Spec.Volumes {
+		if vol.Name == "host-modules" {
+			volumeExists = true
+			break
+		}
+	}
+
+	if !volumeExists {
+		hostModulesVolume := corev1.Volume{
+			Name: "host-modules",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/lib/modules",
+					Type: &hostPathType,
+				},
+			},
+		}
+		pod.Spec.Volumes = append(pod.Spec.Volumes, hostModulesVolume)
+	}
+
+	pod.Spec.HostPID = true
+	pod.Spec.HostNetwork = true
+
+	return pod
+}
