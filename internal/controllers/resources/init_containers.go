53d428214307e195f1a5fa17f4500d5ab6153ccb
Yaron Bar <yaron.bar@weka.io>
2025-09-03 12:00:06 +0300

    fix: use DriversLoaderImage when creating drivers-loader pod

--- internal/controllers/resources/init_containers.go
+++ internal/controllers/resources/init_containers.go
@@ -67,3 +67,49 @@ echo "UIO module loaded successfully"
 
 	return pod
 }
+
+// copy weka dist files if drivers-loader image is
+// different from cluster image
+func (f *PodFactory) copyWekaVersionToDriverLoader(pod *v1.Pod) {
+	originalImage := f.container.Spec.Instructions.Payload
+	pod.Spec.Containers[0].Env = append(pod.Spec.Containers[0].Env, v1.EnvVar{
+		Name:  "CLUSTER_IMAGE_NAME",
+		Value: originalImage,
+	})
+
+	sharedVolumeName := "shared-weka-version-data"
+	sharedVolumeMountPath := "/shared-weka-version-data"
+
+	pod.Spec.InitContainers = []v1.Container{
+		{
+			Name:    "init-setup",
+			Image:   originalImage,
+			Command: []string{"sh", "-c"},
+			Args: []string{
+				`
+						mkdir -p /shared-weka-version-data/dist &&
+						cp -r /opt/weka/dist/* /shared-weka-version-data/dist/ && 
+						echo "Init container completed successfully"
+						`,
+			},
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      sharedVolumeName,
+					MountPath: sharedVolumeMountPath,
+				},
+			},
+		},
+	}
+
+	pod.Spec.Containers[0].VolumeMounts = append(pod.Spec.Containers[0].VolumeMounts, v1.VolumeMount{
+		Name:      sharedVolumeName,
+		MountPath: sharedVolumeMountPath,
+	})
+	pod.Spec.Volumes = append(pod.Spec.Volumes, v1.Volume{
+
+		Name: sharedVolumeName,
+		VolumeSource: v1.VolumeSource{
+			EmptyDir: &v1.EmptyDirVolumeSource{},
+		},
+	})
+}
